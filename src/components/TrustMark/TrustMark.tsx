a0cb87cf4db3fb502205f92e50f2b426d20dace7
leonace924 <meobius123@gmail.com>
2021-09-03 06:46:52 +0300

    Add trust mark component

---
+++ src/components/TrustMark/TrustMark.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Icon from "@components/Icons/Icons";
+import styles from "./TrustMark.module.scss";
+
+export interface ITrustMarkProps {
+  icon: string;
+  title?: string;
+  description?: string;
+}
+
+const TrustMark = ({ icon, title, description }: ITrustMarkProps) => {
+  return (
+    <div className={styles.trustMark}>
+      <div className={styles.trustMarkIcon}>
+        <Icon type={icon} />
+      </div>
+
+      <div className={styles.trustMarkTitle}>
+        <h3>{title}</h3>
+
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TrustMark;
