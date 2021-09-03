8379b159e6c8213478ec5a1151961034c8c8559b
leonace924 <meobius123@gmail.com>
2021-09-03 06:48:43 +0300

    Update section name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "@components/Layout/Layout";
 import Hero from "@sections/Home/Hero/Hero";
 import TrustMakers from "@sections/Home/TrustMakers/TrustMakers";
 import Monterey from "@sections/Home/Monterey/Monterey";
-import PerfectEstate from "@sections/Home/PerfectEstate/PerfectEstate";
+import FindHomes from "@sections/Home/FindHomes/FindHomes";
 
 const Home: NextPage = () => {
   return (
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
       <Hero />
       <TrustMakers />
       <Monterey />
-      <PerfectEstate />
+      <FindHomes />
     </Layout>
   );
 };
--- src/sections/Home/FindHomes/FindHomes.tsx
+++ src/sections/Home/FindHomes/FindHomes.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Button } from "@components/Common/Common";
-import styles from "./PerfectEstate.module.scss";
+import styles from "./FindHomes.module.scss";
 
-export interface IPerfectEstateProps {}
+export interface IFindHomesProps {}
 
-const PerfectEstate = ({}: IPerfectEstateProps) => {
+const FindHomes = ({}: IFindHomesProps) => {
   return (
-    <div className={styles.perfectEstate}>
-      <div className={styles.perfectEstateWrapper}>
-        <div className={styles.perfectEstateContent}>
-          <div className={styles.perfectEstateContentText}>
+    <div className={styles.findHomes}>
+      <div className={styles.findHomesWrapper}>
+        <div className={styles.findHomesContent}>
+          <div className={styles.findHomesContentText}>
             <h2>Find where we build our homes</h2>
             <p>
               Our history spans 25 years and during that time we’ve helped
@@ -25,4 +25,4 @@ const PerfectEstate = ({}: IPerfectEstateProps) => {
   );
 };
 
-export default PerfectEstate;
+export default FindHomes;
