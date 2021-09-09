9d8dfcddbcb6f846e75e80d93dc325e674330868
leonace924 <meobius123@gmail.com>
2021-09-09 11:40:34 -0400

    Add EasyBuyPurchase component

---
+++ src/components/EasyBuyPurchase/EasyBuyPurchase.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Image from "next/image";
+import { Button } from "@components/Common/Common";
+import styles from "./EasyBuyPurchase.module.scss";
+
+export interface IEasyBuyPurchaseProps {}
+
+const EasyBuyPurchase = ({}: IEasyBuyPurchaseProps) => {
+  return (
+    <div className={styles.easyBuyPurchase}>
+      <div className={styles.easyBuyPurchaseContent}>
+        <h2>
+          EasyBuy
+          <br />
+          Purchase
+        </h2>
+        <h3>
+          Fixed price,
+          <br /> no progress payments.
+        </h3>
+      </div>
+
+      <div className={styles.easyBuyPurchaseLogo}>
+        <div className={styles.easyBuyPurchaseLogoButton}>
+          <Button color="light" rounded>
+            Get in Touch
+          </Button>
+        </div>
+        <div className={styles.easyBuyPurchaseLogoImage}>
+          <Image
+            src={"/assets/images/home/img-easyBuy-seal-waxMain.png"}
+            alt="easy-buy-mark"
+            width={192}
+            height={190}
+            layout="responsive"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EasyBuyPurchase;
