0eaaa568840fd11a7e7ec632a4c941b3522ac916
leonace924 <meobius123@gmail.com>
2021-09-10 05:02:11 -0400

    Update arrow button and update image button

---
+++ src/components/Common/ArrowButton/ArrowButton.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Icon from "@components/Icons/Icons";
+import styles from "./ArrowButton.module.scss";
+
+export interface IArrowButtonProps {
+  className?: string;
+  label?: string;
+  onClick?: () => void;
+}
+
+const ArrowButton = ({
+  className = "",
+  label,
+  onClick,
+  ...props
+}: IArrowButtonProps) => {
+  return (
+    <button className={`${styles.arrowButton} ${className}`} {...props}>
+      {label}
+      <div className={styles.arrowButtonIcon}>
+        <Icon type="chevron-down" />
+      </div>
+    </button>
+  );
+};
+
+export default ArrowButton;
--- src/components/Common/Common.tsx
+++ src/components/Common/Common.tsx
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button/Button";
+export { default as ArrowButton } from "./ArrowButton/ArrowButton";
 export { default as ImageButton } from "./ImageButton/ImageButton";
 export { default as Select } from "./Select/Select";
