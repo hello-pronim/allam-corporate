af16c6db4e0b4fc8ff65d7b4694d13ac8b29c4c2
Josh Mapley <joshm@JM-Traffic.local>
2021-09-22 16:13:52 +1000

    retirement page initialisation, creation of retirement page hero based on hero createed for home page

---
+++ src/sections/Retirement/Hero/constant.ts
@@ -0,0 +1,8 @@
+export const heroObj = [
+  {
+    imageUrl: "/assets/images/retirement-living/Slide-1.png",
+    imageUrlMobile: "/assets/images/retirement-living/Slide-1-mobile.png",
+    title: "Find the ideal retirement community to match your lifestyle",
+    subtitle: "Our history spans 25 years and during that time we’ve helped thousands of customers find a new home, with homes and estates spread across many of Sydney’s most popular areas",
+  },
+];
