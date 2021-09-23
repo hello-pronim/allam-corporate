cffb2cb717c2372f29827d8c224d38bfdf7daeff
Josh Mapley <joshm@JM-Traffic.local>
2021-09-23 16:19:53 +1000

    property card common component with mock data

---
+++ src/components/PropertyCard/constants.ts
@@ -0,0 +1,32 @@
+export const card = {
+        id: 1,
+        progPercent: 25,
+        moveIn: 'Jan 2022',
+        images: [
+            {
+                id: "img-1",
+                imageUrl: "/assets/images/temp/img-temp-card-1.jpg",
+                width: 492,
+                height: 355,
+              },
+              {
+                  id: 2,
+                  imageUrl: "/assets/images/temp/img-temp-card-2.jpg",
+                  width: 492,
+                  height: 355,
+                },
+              {
+                  id: 3,
+                  imageUrl: "/assets/images/temp/img-temp-card-1.jpg",
+                  width: 492,
+                  height: 355,
+              }, 
+        ],
+        address: "Lot 710, Pozier Road, Edmondson Park  NSW 2174",
+        bed: 3,
+        bath: 3,
+        car: 3,
+        buildSize: '38',
+        landSize: '234',
+        homeDesign: 'LENNOX-5 TG Trad',
+    };
