516398c1627294740ce6982ae7ea7f6a372fae8b
Josh Mapley <joshm@JM-Traffic.local>
2021-10-04 11:39:33 +1100

    about page and components

--- src/components/FullWidthImage/FullWidthImage.tsx
+++ src/components/FullWidthImage/FullWidthImage.tsx
@@ -4,13 +4,29 @@ import css from "@styled-system/css";
 
 export interface FullWidthImageProps {
   image: string;
+  alignment?:
+    | ["left", "top"]
+    | ["left", "center"]
+    | ["left", "bottom"]
+    | ["right", "top"]
+    | ["right", "center"]
+    | ["right", "bottom"]
+    | ["center", "top"]
+    | ["center", "center"]
+    | ["center", "bottom"];
 }
 
-const FullWidthImage = ({ image }: FullWidthImageProps) => {
+const FullWidthImage = ({
+  image,
+  alignment = ["center", "center"],
+}: FullWidthImageProps) => {
   return (
     <div
       className={styles.FullWidthImage}
-      css={css({ backgroundImage: `url(${image})` })}
+      css={css({
+        backgroundImage: `url(${image})`,
+        backgroundPosition: `${alignment[0]} ${alignment[1]}`,
+      })}
     ></div>
   );
 };
