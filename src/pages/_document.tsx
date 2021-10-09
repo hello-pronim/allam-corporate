55278b4b1e37618ab6529e2664e0346aad2d2d1b
leonace924 <meobius123@gmail.com>
2021-10-09 11:07:52 -0400

    feat: add simple mapbox and fix math.div warnings

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,6 +28,10 @@ class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
           />
           <link rel="stylesheet" href="https://use.typekit.net/ngi4krw.css" />
+          <link
+            href="https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
