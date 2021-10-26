3d282e056f7c33c4fa1b3b00c47245256b654617
leonace924 <meobius123@gmail.com>
2021-10-26 04:23:22 -0400

    chore: update tsconfig and next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["s3.ap-southeast-2.amazonaws.com"],
+    deviceSizes: [640, 768, 1024, 1280, 1536],
+  },
+};
