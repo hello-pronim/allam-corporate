c1fcc3e6dbef77bfeb4b5f913e77f8dc2c34703f
leonace924 <meobius123@gmail.com>
2021-11-18 11:47:04 -0500

    feat: update nextjs and config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  concurrentFeatures: true,
   images: {
     domains: ["s3.ap-southeast-2.amazonaws.com"],
     deviceSizes: [640, 768, 1024, 1280, 1536],
