a6bfc5e5e939bfb317d9a89348622f1096ed9f43
leonace924 <meobius123@gmail.com>
2021-11-18 12:02:47 -0500

    fix: download nextjs version

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
-  concurrentFeatures: true,
   images: {
     domains: ["s3.ap-southeast-2.amazonaws.com"],
     deviceSizes: [640, 768, 1024, 1280, 1536],
