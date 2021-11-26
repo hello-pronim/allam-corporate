803152a4804c9ce24abfe517596dcc69977465dd
leonace924 <meobius123@gmail.com>
2021-11-26 09:17:28 -0500

    feat: add navigation and footer query

--- src/libs/queries.ts
+++ src/libs/queries.ts
@@ -85,3 +85,52 @@ export const easyBuyPurchaseQuery = gql`
     }
   }
 `;
+
+export const navigationQuery = gql`
+  query navigation {
+    globalSet(handle: "mainNavigation") {
+      ... on mainNavigation_GlobalSet {
+        menuItems {
+          ... on menuItems_BlockType {
+            linkName
+            hyperlink
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const footerQuery = gql`
+  query footer {
+    globalSet(handle: "footer") {
+      ... on footer_GlobalSet {
+        heading
+        buttons {
+          ... on buttons_BlockType {
+            buttonLabel
+            buttonLink
+            buttonType
+          }
+        }
+        footerBottom {
+          ... on footerBottom_footerBottom_BlockType {
+            copyrightText
+            bottomLinks {
+              ... on bottomLinks_BlockType {
+                ctaLabel
+                ctaLink
+              }
+            }
+            socialLinks {
+              ... on socialLinks_BlockType {
+                socialName
+                socialLink
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
