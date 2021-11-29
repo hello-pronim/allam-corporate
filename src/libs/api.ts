cb1f61413050f884bbdc11f7239fe069ae61c495
leonace924 <meobius123@gmail.com>
2021-11-29 17:02:19 -0500

    hotfix: update build error

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, DocumentNode, InMemoryCache } from "@apollo/client";
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const getClient = (previewToken: string = undefined) => {
+export const getClient = (previewToken: any = undefined) => {
   return new ApolloClient({
     uri: `${API_URL}${previewToken ? `?token=${previewToken}` : ""}`,
     cache: new InMemoryCache(),
