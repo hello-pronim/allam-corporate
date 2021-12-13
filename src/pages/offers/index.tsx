f4aaa4f05d85414db22936b2be472fdfabafaeb3
leonace924 <meobius123@gmail.com>
2021-12-13 09:13:24 -0500

    feat: update style and add query

--- src/pages/offers/index.tsx
+++ src/pages/offers/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
 import craftAPI from "@libs/api";
+import { gql } from "@apollo/client";
+import { PageProps } from "@models";
 import { layoutQuery } from "@libs/queries";
 import Layout from "@components/Layout/Layout";
 import Hero from "@sections/Offers/Hero/Hero";
 import OffersContent from "@sections/Offers/OffersContent/OffersContent";
-import { PageProps } from "@models";
 
 const PromotionalOffers: NextPage<PageProps> = ({ layoutData }) => {
   return (
@@ -14,6 +15,32 @@ const PromotionalOffers: NextPage<PageProps> = ({ layoutData }) => {
     </Layout>
   );
 };
+
+const offersQuery = gql`
+  query offersQuery {
+    entries(section: "promotions") {
+      ... on promotions_default_Entry {
+        title
+        publishDate
+        expiryDate
+        shortDescription
+        introBlurb
+        description
+        homePageBanner {
+          title
+          url
+        }
+        titleImage {
+          title
+          url
+        }
+        filesDownloads {
+          url
+        }
+      }
+    }
+  }
+`;
 
 export const getStaticProps = async function () {
   const layoutData = await craftAPI(layoutQuery);
