53fc44f4805d7c7f446863fee836031d5086fc46
leonace924 <meobius123@gmail.com>
2022-01-12 08:27:57 -0500

    feat: add map clusters to mapbox

--- src/sections/FindHome/Overview/Overview.tsx
+++ src/sections/FindHome/Overview/Overview.tsx
@@ -1,13 +1,140 @@
-import React from "react";
-import MapboxMap from "@components/MapboxMap/MapboxMap";
+import React, { useCallback, useState, useEffect, useRef } from "react";
+import * as d3 from "d3";
+import Image from "next/image";
+import { useRecoilValue } from "recoil";
+import useSupercluster from "use-supercluster";
+import ReactMapGL, { MapRef, Marker, FlyToInterpolator } from "react-map-gl";
+import { HomeModel } from "@models";
+import { filteredHomes } from "@states/atoms/homes";
+import { useWindowSize } from "@hooks/useWindowSize";
 import styles from "./Overview.module.scss";
 
 export interface IOverviewProps {}
 
 const Overview = ({}: IOverviewProps) => {
+  const homesList: HomeModel[] = useRecoilValue(filteredHomes);
+
+  const { width, height } = useWindowSize();
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: Number(homesList[0].latitude),
+    longitude: Number(homesList[0].longitude),
+    zoom: 8,
+    transitionDuration: 5000,
+    transitionInterpolator: new FlyToInterpolator(),
+    transitionEasing: d3.easeCubic,
+  });
+
+  const mapRef = useRef<MapRef>(null);
+
+  const updateViewport = useCallback(() => {
+    setViewport({
+      ...viewport,
+      width: "100%",
+      height: "100%",
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [width, height]);
+
+  useEffect(() => {
+    updateViewport();
+  }, [updateViewport]);
+
+  const points = homesList.map((home) => ({
+    type: "Feature",
+    properties: { cluster: false, clusterId: home.slug },
+    geometry: {
+      type: "Point",
+      coordinates: [Number(home.longitude), Number(home.latitude)],
+    },
+  }));
+
+  const bounds = mapRef.current
+    ? mapRef.current.getMap().getBounds().toArray().flat()
+    : null;
+
+  const { clusters, supercluster } = useSupercluster({
+    points,
+    bounds,
+    zoom: viewport.zoom,
+    options: { radius: 75, maxZoom: 20 },
+  });
+
+  console.log(clusters);
+
   return (
     <div className={styles.overview}>
-      <MapboxMap />
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        onViewportChange={setViewport}
+        ref={mapRef}
+      >
+        {clusters.map((cluster) => {
+          const [longitude, latitude] = cluster.geometry.coordinates;
+          const { cluster: isCluster, point_count: pointCount } =
+            cluster.properties;
+
+          if (isCluster) {
+            return (
+              <Marker
+                key={`cluster-${cluster.id}`}
+                latitude={latitude}
+                longitude={longitude}
+              >
+                <div
+                  className={styles.cluster}
+                  onClick={() => {
+                    const expansionZoom = Math.min(
+                      supercluster.getClusterExpansionZoom(cluster.id),
+                      20
+                    );
+
+                    setViewport({
+                      ...viewport,
+                      latitude,
+                      longitude,
+                      zoom: expansionZoom,
+                      transitionInterpolator: new FlyToInterpolator({
+                        speed: 2,
+                      }),
+                    });
+                  }}
+                >
+                  <div className={styles.clusterMarker}>
+                    <Image
+                      src={"/assets/icons/icon-union.svg"}
+                      alt={cluster.properties.clusterId}
+                      width={40}
+                      height={52}
+                    />
+                    <span>{pointCount}</span>
+                  </div>
+                </div>
+              </Marker>
+            );
+          }
+
+          return (
+            <Marker
+              key={`crime-${cluster.properties.clusterId}`}
+              latitude={latitude}
+              longitude={longitude}
+            >
+              <div style={{ transform: `translate(-20px, -26px)` }}>
+                <Image
+                  src={"/assets/icons/icon-pin.svg"}
+                  alt={cluster.properties.clusterId}
+                  width={40}
+                  height={52}
+                />
+              </div>
+            </Marker>
+          );
+        })}
+      </ReactMapGL>
     </div>
   );
 };
