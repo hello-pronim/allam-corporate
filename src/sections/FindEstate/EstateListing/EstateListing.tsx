1ba5cb78787207576da84d1ff681d8e7a3ca927f
leonace924 <meobius123@gmail.com>
2022-01-15 03:20:55 -0500

    fix: update load more functionality to estate main page

--- src/sections/FindEstate/EstateListing/EstateListing.tsx
+++ src/sections/FindEstate/EstateListing/EstateListing.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import Link from "next/link";
 import { useRecoilValue } from "recoil";
 
@@ -14,14 +14,20 @@ export interface IEstateListingProps {
 }
 
 const EstateListing = ({ homesList }: IEstateListingProps) => {
+  const MAX_ESTATE_COUNT = 12;
   const estatesList = useRecoilValue(filteredEstates);
+  const [isLoadMore, setIsLoadMore] = useState(false);
+
+  const visibleEstates = useMemo(() => {
+    return isLoadMore ? estatesList : estatesList.slice(0, MAX_ESTATE_COUNT);
+  }, [isLoadMore, estatesList]);
 
   return (
     <div className={styles.estateListing}>
       <div className={styles.estateListingWrapper}>
         <div className={styles.estateListingView}>
           <div className={styles.estateListingCards}>
-            {estatesList?.map((estate, id) => (
+            {visibleEstates?.map((estate, id) => (
               <Link href={`/find-estate/${estate.slug}`} key={id}>
                 <a>
                   <EstateCard estate={estate} homesList={homesList} />
@@ -31,11 +37,13 @@ const EstateListing = ({ homesList }: IEstateListingProps) => {
             <EasyBuyPurchase />
           </div>
 
-          <div className={styles.estateListingViewCTA}>
-            <Button size="large" rounded>
-              Load more
-            </Button>
-          </div>
+          {!isLoadMore && (
+            <div className={styles.estateListingViewCTA}>
+              <Button size="large" onClick={() => setIsLoadMore(true)} rounded>
+                Load more
+              </Button>
+            </div>
+          )}
         </div>
       </div>
     </div>
