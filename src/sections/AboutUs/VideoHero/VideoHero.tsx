19145bc208f4d212cd990534f2cd62d330f8107f
leonace924 <meobius123@gmail.com>
2022-02-17 05:20:13 -0500

    update: about us page hero video control

--- src/sections/AboutUs/VideoHero/VideoHero.tsx
+++ src/sections/AboutUs/VideoHero/VideoHero.tsx
@@ -18,18 +18,20 @@ const VideoHero = ({ data }: VideoHeroProps) => {
           className={styles.heroImage}
           style={{ backgroundImage: `url(${data?.titleImage?.[0]?.url})` }}
         >
-          <div
-            className={styles.heroImagePlayIcon}
-            onClick={() =>
-              setVideoModal({
-                isOpen: true,
-                videoUrl: data?.videoLink,
-                coverImageUrl: data?.titleImage?.[0]?.url,
-              })
-            }
-          >
-            <Icon type="video-play" /> <h4>Watch our story</h4>
-          </div>
+          {data?.videoLink && (
+            <div
+              className={styles.heroImagePlayIcon}
+              onClick={() =>
+                setVideoModal({
+                  isOpen: true,
+                  videoUrl: data?.videoLink,
+                  coverImageUrl: data?.titleImage?.[0]?.url,
+                })
+              }
+            >
+              <Icon type="video-play" /> <h4>Watch our story</h4>
+            </div>
+          )}
         </div>
 
         <div className={styles.heroContent}>
